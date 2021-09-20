07883dc241072f3afb45d617831b6a2090dce8db
tbowenmsu <81266672+tbowenmsu@users.noreply.github.com>
2021-09-20 18:53:27 -0400

    finished code push

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,18 +1,14 @@
-// use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
 class AuthService {
-  // get user data
   getProfile() {
     return decode(this.getToken());
   }
 
   // check if user's logged in
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token); 
   }
 
   // check if token is expired
@@ -28,7 +24,7 @@ class AuthService {
   }
 
   getToken() {
-    // Retrieves the user token from localStorage
+    // Gets token from storage
     return localStorage.getItem('id_token');
   }
 
@@ -41,7 +37,6 @@ class AuthService {
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
 }
